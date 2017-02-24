aed411fe5de8a915809fbf7c3e19efdb3e8da6dd
zhangbing <zhangbingbeyondbit@163.com>
2017-02-24 18:45:09 +0800

    tx

--- battle-03/src/test/java/com/battle/Battle03ApplicationTests.java
+++ battle-03/src/test/java/com/battle/Battle03ApplicationTests.java
@@ -1,16 +1,12 @@
 package com.battle;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Battle03ApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
