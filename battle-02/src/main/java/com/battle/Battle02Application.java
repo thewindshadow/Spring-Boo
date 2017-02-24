47eeff108c060619bb33a40a6f49d10cae0810bf
zhangbing <zhangbingbeyondbit@163.com>
2017-02-24 16:33:55 +0800

    tx

---
+++ battle-02/src/main/java/com/battle/Battle02Application.java
@@ -0,0 +1,12 @@
+package com.battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Battle02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Battle02Application.class, args);
+	}
+}
---
+++ battle-02/src/main/java/com/battle/service/BattleService.java
@@ -0,0 +1,92 @@
+package com.battle.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Bing.Z on 2017/2/24.
+ */
+@Component
+public class BattleService {
+    @Value("${com.battle.blog.name}")
+    private String name;
+    @Value("${com.battle.blog.title}")
+    private String title;
+    @Value("${com.battle.blog.desc}")
+    private String desc;
+
+    @Value("${com.battle.blog.value}")
+    private String value;
+    @Value("${com.battle.blog.number}")
+    private Integer number;
+    @Value("${com.battle.blog.bignumber}")
+    private Long bignumber;
+    @Value("${com.battle.blog.test1}")
+    private Integer test1;
+    @Value("${com.battle.blog.test2}")
+    private Integer test2;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Long getBignumber() {
+        return bignumber;
+    }
+
+    public void setBignumber(Long bignumber) {
+        this.bignumber = bignumber;
+    }
+
+    public Integer getTest1() {
+        return test1;
+    }
+
+    public void setTest1(Integer test1) {
+        this.test1 = test1;
+    }
+
+    public Integer getTest2() {
+        return test2;
+    }
+
+    public void setTest2(Integer test2) {
+        this.test2 = test2;
+    }
+}
