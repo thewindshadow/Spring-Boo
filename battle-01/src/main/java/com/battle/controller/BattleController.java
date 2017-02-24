5bb6cfb12696e209c8759d750fa7e80972164d78
zhangbing <zhangbingbeyondbit@163.com>
2017-02-24 17:24:11 +0800

    tx

---
+++ battle-01/src/main/java/com/battle/controller/BattleController.java
@@ -0,0 +1,16 @@
+package com.battle.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by bing.z on 2017/2/24.
+ */
+@RestController
+public class BattleController {
+
+    @RequestMapping(value = "index")
+    public String index() {
+        return "Hello World!";
+    }
+}
