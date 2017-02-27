578c8c338cf6fb9d6b63d7da9bca4f201d58a19b
zhangbing <zhangbingbeyondbit@163.com>
2017-02-27 16:27:46 +0800

    FreeMarker模板引擎

---
+++ battle-05/src/main/java/com/battle/controller/BattleController.java
@@ -0,0 +1,25 @@
+package com.battle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by Bing.Z on 2017/2/27.
+ */
+@Controller
+public class BattleController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/")
+    public String index(ModelMap map) {
+        map.addAttribute("host", "https://www.baidu.com");
+        return "index";
+    }
+}
