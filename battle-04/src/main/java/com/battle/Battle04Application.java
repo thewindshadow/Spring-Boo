3c5a6ae08c7b191ffeac58c2ed08d57fae5cf86d
zhangbing <zhangbingbeyondbit@163.com>
2017-02-27 15:20:31 +0800

    tx

---
+++ battle-04/src/main/java/com/battle/Battle04Application.java
@@ -0,0 +1,12 @@
+package com.battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Battle04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Battle04Application.class, args);
+	}
+}
