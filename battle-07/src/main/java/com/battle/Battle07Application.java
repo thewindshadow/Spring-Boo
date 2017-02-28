74675f87d5038e15da9653e6645d106a07691509
zhangbing <zhangbingbeyondbit@163.com>
2017-02-28 09:39:22 +0800

    Swagger2 Restful API

--- battle-07/src/main/java/com/battle/Battle07Application.java
+++ battle-07/src/main/java/com/battle/Battle07Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Battle07Application {
-
+	// 启动成功  通过  http://localhost:8080/swagger-ui.html 访问 restful API
 	public static void main(String[] args) {
 		SpringApplication.run(Battle07Application.class, args);
 	}
