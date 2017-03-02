d042678f75239803e7f797b7c8f8b8654d73e4cc
zhangbing <zhangbingbeyondbit@163.com>
2017-03-02 11:20:45 +0800

    整合MyBatis

---
+++ battle-15/src/main/java/com/battle/domain/UserMapper.java
@@ -0,0 +1,19 @@
+package com.battle.domain;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by Bing.Z on 2017/3/2.
+ */
+@Mapper
+public interface UserMapper {
+
+    @Select("SELECT * FROM USER WHERE NAME = #{name}")
+    User findByName(@Param("name") String name);
+
+    @Insert("INSERT INTO USER(NAME, AGE) VALUES(#{name}, #{age})")
+    int insert(@Param("name") String name, @Param("age") Integer age);
+}
---
+++ battle-15/src/test/java/com/battle/Battle15ApplicationTests.java
@@ -0,0 +1,31 @@
+package com.battle;
+
+import com.battle.domain.User;
+import com.battle.domain.UserMapper;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Battle15Application.class)
+@Transactional
+public class Battle15ApplicationTests {
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Test
+	@Rollback
+	public void findByName() throws Exception {
+		userMapper.insert("AAA", 20);
+		User u = userMapper.findByName("AAA");
+		System.out.println(u.getAge()+"\t"+u.getName());
+		Assert.assertEquals(20, u.getAge().intValue());
+	}
+
+}
