c339ce752d05adb9f473a220f7ce51a3cb32ad24
zhangbing <zhangbingbeyondbit@163.com>
2017-03-07 10:31:21 +0800

    使用Spring Security进行安全控制

---
+++ battle-25/src/main/java/com/battle/controller/BattleController.java
@@ -0,0 +1,27 @@
+package com.battle.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Bing.Z on 2017/3/7.
+ */
+@Controller
+public class BattleController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login() {
+        return "login";
+    }
+
+}
