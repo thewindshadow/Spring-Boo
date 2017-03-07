ce0adf7199d548b8834b664453e59f322730cac5
zhangbing <zhangbingbeyondbit@163.com>
2017-03-07 11:18:37 +0800

    缓存支持：注解配置与EhCache使用

---
+++ battle-26/src/test/java/com/battle/Battle26ApplicationTests.java
@@ -0,0 +1,40 @@
+package com.battle;
+
+import com.battle.domain.User;
+import com.battle.domain.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Battle26ApplicationTests {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Before
+	public void before() {
+		userRepository.save(new User("AAA", 10));
+	}
+
+	@Test
+	public void test() throws Exception {
+
+		User u1 = userRepository.findByName("AAA");
+		System.out.println("第一次查询：" + u1.getAge());
+
+		User u2 = userRepository.findByName("AAA");
+		System.out.println("第二次查询：" + u2.getAge());
+
+		u1.setAge(20);
+		userRepository.save(u1);
+		User u3 = userRepository.findByName("AAA");
+		System.out.println("第三次查询：" + u3.getAge());
+
+	}
+
+}
