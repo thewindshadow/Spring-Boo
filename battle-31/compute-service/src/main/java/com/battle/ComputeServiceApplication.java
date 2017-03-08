08575581a0e6d287a8c4a56c44852578f62b7f85
zhangbing <zhangbingbeyondbit@163.com>
2017-03-08 18:05:23 +0800

    SpringCloud服务注册与发现

---
+++ battle-31/compute-service/src/main/java/com/battle/ComputeServiceApplication.java
@@ -0,0 +1,15 @@
+package com.battle;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ComputeServiceApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(ComputeServiceApplication.class, args);
+		new SpringApplicationBuilder(ComputeServiceApplication.class).web(true).run(args);
+	}
+}
