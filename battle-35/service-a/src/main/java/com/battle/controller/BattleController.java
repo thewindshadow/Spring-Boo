dfb8879abe784e9275e6e68f9eaacd80b1847ae3
zhangbing <zhangbingbeyondbit@163.com>
2017-03-24 16:40:12 +0800

    Spring Cloud构建微服务架构（五）服务网关

---
+++ battle-35/service-a/src/main/java/com/battle/controller/BattleController.java
@@ -0,0 +1,31 @@
+package com.battle.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Bing.Z on 2017/3/13.
+ */
+@RestController
+public class BattleController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/add" ,method = RequestMethod.GET)
+    public String add(@RequestParam Integer a, @RequestParam Integer b) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        Integer r = a + b;
+        logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
+        return "From Service-A, Result is " + r;
+    }
+
+}
