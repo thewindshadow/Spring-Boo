4e7a3f1f9b2b2f8416798847d4626624d42bf03b
zhangbing <zhangbingbeyondbit@163.com>
2017-03-28 09:28:57 +0800

    Spring Boot整合Dubbo

---
+++ battle-36/compute-service/src/main/java/com/battle/service/ComputeService.java
@@ -0,0 +1,10 @@
+package com.battle.service;
+
+/**
+ * Created by Bing.Z on 2017/3/24.
+ */
+public interface ComputeService {
+
+    Integer add(int a, int b);
+
+}
---
+++ battle-36/consumer/src/main/java/com/battle/service/ComputeService.java
@@ -0,0 +1,8 @@
+package com.battle.service;
+
+/**
+ * Created by Bing.Z on 2017/3/24.
+ */
+public interface ComputeService {
+    Integer add(int a, int b);
+}
