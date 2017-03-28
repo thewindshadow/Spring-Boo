b3bda477ce86bbef341e44eb8fe3d48ad1aabeda
zhangbing <zhangbingbeyondbit@163.com>
2017-03-28 15:49:24 +0800

    spring boot与dubbo的API依赖管理

---
+++ battle-37/compute-service/compute-api-server/src/main/java/com/battle/service/impl/ComputeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.battle.service.impl;
+
+import com.battle.service.ComputeService;
+
+/**
+ * Created by Bing.Z on 2017/3/28.
+ */
+public class ComputeServiceImpl implements ComputeService {
+
+    @Override
+    public Integer add(int a, int b) {
+        return a+b;
+    }
+}
---
+++ battle-37/compute-service/compute-api/src/main/java/com/battle/service/ComputeService.java
@@ -0,0 +1,9 @@
+package com.battle.service;
+
+/**
+ * Created by Bing.Z on 2017/3/28.
+ */
+public interface ComputeService  {
+
+    Integer add(int a, int b);
+}
